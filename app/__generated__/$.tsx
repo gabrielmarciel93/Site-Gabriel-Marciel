bd4cf7de4bf636f389918aefabe9729e3b111bbf
Gabriel Marciel <gabriel.marciel93@gmail.com>
2025-10-20 18:17:06 -0300

    Initial commit

---
+++ app/__generated__/$.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable */
+      /* This is a auto generated file for building the project */ 
+
+
+      import { Fragment, useState } from "react";
+      import { useResource, useVariableState } from "@webstudio-is/react-sdk/runtime";
+      import { Body as Body, Link as Link } from "@webstudio-is/sdk-components-react-router";
+import { HtmlEmbed as HtmlEmbed } from "@webstudio-is/sdk-components-react";
+
+
+      export const projectId = "8816130d-d432-4ad2-8116-915be45ca807";
+
+      export const lastPublished = "2025-10-20T20:58:13.852Z";
+
+      export const siteName = undefined;
+
+      export const breakpoints = [{"id":"Xb3sqS8dgX7awUL3SLT0r"},{"id":"qFEdqDttMrHqhWgF_js9t","maxWidth":991},{"id":"UsDiXwN3YjLyaoHoDwHnZ","maxWidth":767},{"id":"9GW-_38PT6gPyAUHp8fEp","maxWidth":479}];
+
+      export const favIconAsset: string | undefined =
+        undefined;
+
+      // Font assets on current page (can be preloaded)
+      export const pageFontAssets: string[] =
+        ["DMSans-VariableFont_opsz_wght_0nlVRsSMOXtybRd9LDPDS.ttf"]
+
+      export const pageBackgroundImageAssets: string[] =
+        []
+
+      
+
+      const Page = (_props: { system: any; }) => {
+return <Body
+className={`w-element c1ij51x0 c18n3o80 c1a4zjes cghj4pv`}>
+<div
+className={`w-element`}>
+<div
+className={`w-element c4cs9kk cfo00z7 c19aomgz c1fwnqp9 c530lg9 c1us4q23`}>
+<div
+className={`w-element`}>
+{"404"}
+</div>
+<div
+className={`w-element c16wy52z c1lygkdd c1yackvh cejpodb c1pf4z8f cp8c5kc`}>
+{"404"}
+</div>
+<div
+className={`w-element c16wy52z c1lygkdd cn4womu cejpodb cynd89v cp8c5kc`}>
+{"404"}
+</div>
+<div
+className={`w-element c16wy52z c1ctshrq chilnvq cr6ud55 cghj4pv c1nsjg3m`} />
+</div>
+<p
+className={`w-element c11a1oz6 ch2vy06 cn1f0iv c1ol8iu0 c7if4l1`}>
+{"PAGE NOT FOUND"}
+</p>
+</div>
+<Link
+href={"https://webstudio.is/?via=badge"}
+target={"_blank"}
+className={`w-element c14w5203 cjczsls ctjdz1f c1a4zjes c18n3o80 c1ndp2is c8vaqc7 c12jelf0 cb1zu2a cu2wqyl c1c76fqy c1x60vc4 cgyy5tz c1dhql52 c1p30eyj c1y5vznx caj7ff2 c530lg9 c1lycng8 c1ympiav cujb0b4 c1dzh6zo cq92p79 cbvgw49 c11v9l0t cl65zgv czykhzb cf1dusu`}>
+<HtmlEmbed
+code={"<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"currentColor\" viewBox=\"0 0 16 16\" width=\"100%\" height=\"100%\" style=\"display: block;\"><path fill-rule=\"evenodd\" d=\"m12.32 12.416 2.62-8.085a1.205 1.205 0 1 0-2.292-.746l-2.62 8.084a1.205 1.205 0 1 0 2.292.747Z\" clip-rule=\"evenodd\"/><path fill-rule=\"evenodd\" d=\"M8 7.624c.297 0 .517.175.704.394.207.243.373.545.514.866.634 1.44.753 3.241.753 3.241a1.206 1.206 0 0 0 1.285 1.122 1.207 1.207 0 0 0 1.12-1.287s-.16-2.25-.952-4.05C10.744 6.364 9.594 5.208 8 5.208c-1.594 0-2.744 1.156-3.424 2.7-.792 1.8-.951 4.05-.951 4.05a1.207 1.207 0 0 0 1.12 1.288 1.206 1.206 0 0 0 1.284-1.122s.119-1.8.753-3.24a3.52 3.52 0 0 1 .514-.867c.187-.22.406-.394.704-.394Z\" clip-rule=\"evenodd\"/><path fill-rule=\"evenodd\" d=\"M5.973 11.669 3.352 3.585a1.205 1.205 0 1 0-2.293.746l2.622 8.084a1.205 1.205 0 1 0 2.292-.746Z\" clip-rule=\"evenodd\"/></svg>"}
+className={`w-html-embed c74ily4 c8ogacn crio97g ckh31ih`} />
+<div
+className={`w-element`}>
+{"Built with Webstudio"}
+</div>
+</Link>
+</Body>
+}
+
+
+      export { Page }
+    
